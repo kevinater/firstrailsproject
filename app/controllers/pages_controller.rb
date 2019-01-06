fc0bb866c5f9a52860cb1e9291add40adc3f04be
Kevin Vahdat <kvahdat@nyc.yearup.org>
2019-01-06 22:25:54 +0000

    Created post form and routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,9 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def new
+  end
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
-  get 'pages/about'
-  get 'pages/contact'
-  get 'pages/home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/pages/:about" => "pages#show"
+#   get 'pages/about'
+#   get 'pages/contact'
+#   get 'pages/home'
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#   get "/pages/:about" => "pages#show"
+
+root 'posts#index'
+
+
+
+
+
+resources :posts
+
 
 end
+
